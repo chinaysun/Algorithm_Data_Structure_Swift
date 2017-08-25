e91a563ff1baa17d39320e49059d556ce11104df
chinaysun <chinaysun@gmail.com>
2017-08-25 23:18:15 +1000

    Add Insert Function to Max Heap

--- Data_Structure/MaxHeap.swift
+++ Data_Structure/MaxHeap.swift
@@ -71,20 +71,66 @@ func extract_maximum(array:inout [Int])->Int
         return -1
     }
         
-    var max = array[1]
+    let max = array[1]
     array[1] = array[array.count-1]
+    array[0] = array[0] - 1
     array.remove(at: 1)
     max_heapify(array: &array, root: 1, numberOfElements: array.count-1)
     return max
         
 }
 
-let myArray = [2,3,5,4,6,1,0]
-let myHeapArray = build_maxHeap(array: myArray)
-        
-for i in 1...(myHeapArray.count - 1)
+func increase_value (array:inout [Int], index:Int,val:Int)
 {
-    print(myHeapArray[i],terminator: " ")
+
+    array[index] = val
+    var currentIndex = index
+    
+    
+    
+    while( currentIndex > 1 && array[currentIndex/2] < array[currentIndex])
+    {
+        swap(array: &array,index1:currentIndex/2,index2:currentIndex)
+        currentIndex = currentIndex/2
+    }
+}
+
+func insert_value (array:inout [Int], val:Int)
+{
+    array[0] = array[0] + 1
+    array.append(-1) 
+    increase_value (array:&array, index:array[0],val:val)
+}
+
+
+var testcase = Int(readLine()!)!
+var originalArrayString = readLine()!.components(separatedBy: " ")
+var originalArray = [Int]()
+
+for elements in originalArrayString
+{
+    originalArray.append(Int(elements)!)
+}
+
+var myMaxHeap = build_maxHeap(array:originalArray)
+
+var operationsCase = Int(readLine()!)!
+
+for i in 1...operationsCase
+{
+    var operation = readLine()!.components(separatedBy: " ")
+    
+    if Int(operation.first!)! == 1
+    {
+        var val = Int(operation.last!)! 
+        insert_value (array:&myMaxHeap, val:val)
+    }
+    
+    if Int(operation.first!)! == 2
+    {
+        print(extract_maximum(array:&myMaxHeap))
+    }
+    
 }
 
 
